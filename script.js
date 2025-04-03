7228d20b848a1a62c2c9c16481b230b10420a04f
Kauã Nantes <162463188+nantesfx@users.noreply.github.com>
2025-04-03 09:31:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "red";
+ctx.arc(150, 150, 100, 0, 2*Math.PI);
+ctx.stroke();
+
+ctx.strokeStyle = "red";
+ctx.beginPath()
+ctx.arc(450, 150, 100, 0, 2*Math.PI);
+ctx.stroke();
+ctx.fill();
+ctx.fillStyle = "red";
+
+ctx.strokeStyle = "black";
+ctx.beginPath()
+ctx.arc(300, 270, 100, 0, 1*Math.PI);
+ctx.stroke();
